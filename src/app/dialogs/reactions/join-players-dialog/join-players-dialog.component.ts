541fc1ba22f9cdbae1c835742b6307c83ba0b2e4
AshutoshPaul <ashutosh.paul@altorumleren.com>
2022-10-12 16:02:32 +0530

    3 2 1 counter

--- src/app/dialogs/reactions/join-players-dialog/join-players-dialog.component.ts
+++ src/app/dialogs/reactions/join-players-dialog/join-players-dialog.component.ts
@@ -1,5 +1,8 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Observable, of, interval, timer } from 'rxjs';
+import { take, concatMapTo } from 'rxjs/operators';
+import { SubSink } from 'subsink';
 
 interface DialogData {
   joinedPlayers: number;
@@ -11,11 +14,20 @@ interface DialogData {
   templateUrl: './join-players-dialog.component.html',
   styleUrls: ['./join-players-dialog.component.scss']
 })
-export class JoinPlayersDialogComponent implements OnInit {
+export class JoinPlayersDialogComponent implements OnInit, OnDestroy {
 
   message: string = 'Waiting for players to join...';
+  startMessage: string = 'Game starts in';
+
   joinedPlayers: number;
   totalPlayers: number;
+  isStartGame: boolean = false;
+  isStartGame$: Observable<boolean>;
+
+  secondsCounter: number = 3;
+  secondsCounter$: Observable<number>;
+
+  private readonly _subSink = new SubSink();
 
   constructor(
     private readonly _dialogRef: MatDialogRef<JoinPlayersDialogComponent>,
@@ -23,8 +35,33 @@ export class JoinPlayersDialogComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this._dialogRef.disableClose = true;
     this.joinedPlayers = this._data.joinedPlayers;
     this.totalPlayers = this._data.totalPlayers;
+  }
+
+  startGameAlert(): void {
+    this.isStartGame = true;
+    this.isStartGame$ = of(this.isStartGame);
+
+    const timer$ = timer(0);
+    const interval$ = interval(1000).pipe(take(3));
+    const timing$ = timer$.pipe(concatMapTo(interval$));
+
+    this.secondsCounter$ = of(this.secondsCounter);
+    this._subSink.add(
+      timing$.subscribe(_ => {
+        this.secondsCounter$ = of(--this.secondsCounter);
+      })
+    );
+
+    setTimeout(() => {
+      this._dialogRef.close();
+    }, 3000);
+  }
+
+  ngOnDestroy(): void {
+    this._subSink.unsubscribe();
   }
 
 }
