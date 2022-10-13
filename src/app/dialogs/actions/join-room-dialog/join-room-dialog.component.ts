7b84dceeedeb94a0259e940df8cd76ef4aebe565
AshutoshPaul <ashutosh.paul@altorumleren.com>
2022-10-13 23:11:07 +0530

    join room basic commit

--- src/app/dialogs/actions/join-room-dialog/join-room-dialog.component.ts
+++ src/app/dialogs/actions/join-room-dialog/join-room-dialog.component.ts
@@ -1,5 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { FormControl, FormBuilder, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { CreateRoomDialogComponent } from '../create-room-dialog/create-room-dialog.component';
 
 interface DialogData {
   rooms: string[];
@@ -12,12 +14,39 @@ interface DialogData {
 })
 export class JoinRoomDialogComponent implements OnInit {
 
+  roomControl: FormControl;
+
   constructor(
-    private readonly _dialogRef: MatDialogRef<JoinRoomDialogComponent>,
+    private readonly _dialogRef: MatDialogRef<CreateRoomDialogComponent>,
     @Inject(MAT_DIALOG_DATA) private readonly _data: DialogData,
+    private readonly _formBuilder: FormBuilder,
   ) { }
 
   ngOnInit(): void {
+    this.roomControl = this._formBuilder.control('', Validators.required);
+  }
+
+  joinRoom(): void {
+    if(!this.isInvalid) {
+      console.log('create room');
+    }
+  }
+
+  close(): void {
+    this._dialogRef.close();
+  }
+
+  get isInvalid(): boolean {
+    return this.roomControl.invalid;
+  }
+
+  get isDirty(): boolean {
+    return this.roomControl.dirty;
+  }
+
+  get errorMessage(): string {
+    if (this.roomControl.hasError("required")) { return "Enter value"; }
+    return null;
   }
 
 }
