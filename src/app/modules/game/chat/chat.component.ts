ad9a26b2043972a349e27dd1bce47e9761a65629
AshutoshPaul <ashutosh.paul@altorumleren.com>
2022-10-16 20:17:13 +0530

    scroll bottom

--- src/app/modules/game/chat/chat.component.ts
+++ src/app/modules/game/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewChecked, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { IMessage } from 'src/app/core/interfaces/message.interface';
@@ -8,9 +8,11 @@ import { IMessage } from 'src/app/core/interfaces/message.interface';
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss']
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit, AfterViewChecked {
   
   @Input() message$: Observable<IMessage>;
+
+  @ViewChild('messages') elRef: ElementRef;
   
   messages: IMessage[] = [];
   messages$: Observable<IMessage[]>;
@@ -29,6 +31,10 @@ export class ChatComponent implements OnInit {
     });
   }
 
+  ngAfterViewChecked(): void {
+    this._scrollToBottom();
+  }
+
   sendMessage(): void {
     console.log(this.message);
     this._clearMessage();
@@ -36,6 +42,10 @@ export class ChatComponent implements OnInit {
 
   private _clearMessage() {
     this.message = '';
+  }
+
+  private _scrollToBottom() {
+    this.elRef.nativeElement.scrollTop = this.elRef.nativeElement.scrollHeight;
   }
 
 }
